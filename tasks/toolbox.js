80c7d051ab79890482054b8ff5e837faea8e268f
Raffaele Brivio <raffaele.brivio@dev4side.com>
2022-06-14 17:32:34 +0200

    Fix tool:keyburner:burn task

--- tasks/toolbox.js
+++ tasks/toolbox.js
@@ -141,106 +141,42 @@ task('tool:keyburner:burn', 'mint some keys and burn them ??')
   .addParam('keyburnaddr', 'the key burner address')
   .addParam('lockaddr', 'the public lock address')
   .addParam('tokenid', 'the key tokenid')
-  .setAction(async ({ keyburnaddr, lockaddr, tokenid }, { ethers }) => {
-    if (keydest == null) {
-      let [user1] = await ethers.getSigners()
-      keydest = user1.address;
-    }
+  .addParam('eventhash', 'the event hash')
+  .setAction(async ({ keyburnaddr, lockaddr, tokenid, eventhash }, { ethers }) => {
+    const [signer] = await ethers.getSigners()
 
-    let readlock = await ethers.getContractAt('PublicLock', lockaddr)
-    const ownerAddress = await readlock.ownerOf(tokenid)
+    const KeyBurner = await ethers.getContractFactory('EventKeyBurner')
+    const keyBurner = KeyBurner.attach(keyburnaddr)
 
+    const outwave = await ethers.getContractAt('OutwaveEvent', await keyBurner.readOutwave())
+    console.log(`outwave instantiated`)
 
-    console.log("owner is aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa ")
-    console.log(ownerAddress)
+    // purchase key from lock
+    const publicLock = await ethers.getContractAt('PublicLock', lockaddr)
 
+    // approve key trasfer from keyBurner in publickLock
+    await publicLock.approve(keyBurner.address, tokenid)
+    console.log(
+      `One time trasfer of ERC721 '${tokenid}:${publicLock.address}' has been granted to keyBurner ${keyBurner.address}`
+    )
 
+    // burn the token
+    const txBurn = await keyBurner.connect(signer).burnKey(publicLock.address, tokenid, eventhash)
+    const txBurnRec = await txBurn.wait()
+    const keyBurnEvent = txBurnRec.events.find((v) => v.event === 'KeyBurn')
+    const nftMintEvent = txBurnRec.events.find((v) => v.event === 'Transfer' && v.args.from == web3.utils.padLeft(0, 40))
 
-    // const KeyBurner = await ethers.getContractFactory('EventKeyBurner')
-    // const keyBurner = KeyBurner.attach(keyburnaddr)
+    console.log(
+      `Key '${keyBurnEvent.args.tokenId}:${keyBurnEvent.args.lock}' has been burned by keyBurner on behalf of user ${keyBurnEvent.args.from}`
+    )
+    console.log(
+      `User received the OPA NFT #${nftMintEvent.args.tokenId} in return, it's tokenUri is ${await keyBurner.tokenURI(nftMintEvent.args.tokenId)}`
+    )
 
-    // const outwave = await ethers.getContractAt('OutwaveEvent', await keyBurner.readOutwave())
-    // console.log(`outwave instantiated`)
+    const originalKey = await keyBurner.readOriginalKey(nftMintEvent.args.tokenId)
+    console.log('Original Key info:')
+    console.log(originalKey)
 
-    // await outwave.connect(lockOwner).eventSetBaseTokenURI(lockaddr, 'ipfs://QmdBAufFCb7ProgWvWaNkZmeLDdPLXRKF3ku5tpe99vpPx/')
-    // console.log('set new base token uri on public lock')
-
-    // // purchase key from lock
-    // const publicLock = await ethers.getContractAt('PublicLock', lockaddr)
-    // console.log(`public lock instantiated`)
-    // const keyPrice = await publicLock.keyPrice()
-    // // eslint-disable-next-line no-console
-    // console.log(`Key price is ${keyPrice} WEI`)
-
-    // const signers = (await ethers.getSigners()).slice(-10)
-
-    // for (let index = 0; index < signers.length; index++) {
-    //   const signer = signers[index]
-    //   const signerKeyBalance = await publicLock.balanceOf(
-    //     signer.address
-    //   )
-    //   console.log(`Key balance of ${signer.address} is ${signerKeyBalance}`)
-
-    //   if (signerKeyBalance == 1) continue
-
-    //   const txPurchase = await publicLock
-    //     .connect(signer)
-    //     .purchase(
-    //       [],
-    //       [signer.address],
-    //       [web3.utils.padLeft(0, 40)],
-    //       [web3.utils.padLeft(0, 40)],
-    //       [[]],
-    //       {
-    //         value: keyPrice,
-    //       }
-    //     )
-
-    //   const txPurRec = await txPurchase.wait()
-    //   const evt = txPurRec.events.find((v) => v.event === 'Transfer')
-    //   // const tokenId = evt.args.tokenId
-    //   // eslint-disable-next-line no-console
-    //   console.log(`Token ${evt.args.tokenId} minted and sent to ${evt.args.to}`)
-    //   // eslint-disable-next-line no-console
-    //   console.log(
-    //     `Key balance of ${signer.address} is ${await publicLock.balanceOf(
-    //       signer.address
-    //     )}`
-    //   )
-
-    //   console.log('-----------------------------')
-    // }
-
-    // for (let index = 0; index < signers.length; index++) {
-    //   const signer = signers[index]
-    //   const tokenId = await publicLock
-    //     .connect(signer)
-    //     .tokenOfOwnerByIndex(signer.address, 0)
-
-    //   // approve key trasfer from keyBurner in publickLock
-    //   await publicLock.connect(signer).approve(keyBurner.address, tokenId)
-    //   console.log(
-    //     `One time trasfer of ERC721 '${tokenId}:${publicLock.address}' has been granted to keyBurner ${keyBurner.address}`
-    //   )
-
-    //   // burn the token
-    //   const txBurn = await keyBurner.connect(signer).burnKey(publicLock.address, tokenId)
-    //   const txBurnRec = await txBurn.wait()
-    //   const keyBurnEvent = txBurnRec.events.find((v) => v.event === 'KeyBurn')
-    //   const nftMintEvent = txBurnRec.events.find((v) => v.event === 'Transfer' && v.args.from == web3.utils.padLeft(0, 40))
-
-    //   console.log(
-    //     `Key '${keyBurnEvent.args.tokenId}:${keyBurnEvent.args.lock}' has been burned by keyBurner on behalf of user ${keyBurnEvent.args.from}`
-    //   )
-    //   console.log(
-    //     `User received the OPA NFT #${nftMintEvent.args.tokenId} in return, it's tokenUri is ${await keyBurner.tokenURI(nftMintEvent.args.tokenId)}`
-    //   )
-
-    //   const originalKey = await keyBurner.readOriginalKey(nftMintEvent.args.tokenId)
-    //   console.log('Original Key info:')
-    //   console.log(originalKey)
-
-    //   console.log('-----------------------------')
-    // }
+    console.log('-----------------------------')
   })
 /* eslint-enable */
