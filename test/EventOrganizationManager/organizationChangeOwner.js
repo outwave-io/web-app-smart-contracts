344c9a47a6a57925077da4281bd32b5b5ec4bb7b
Giacomo Stelluti Scala <gsscoder@gmail.com>
2022-10-13 07:53:00 +0200

    Implement behavioral test for organizationChangeOwner #12034 #12116

---
+++ test/EventOrganizationManager/organizationChangeOwner.js
@@ -0,0 +1,53 @@
+const { assert } = require('chai')
+const { ethers } = require('hardhat')
+
+contract('Organization Event Manager', () => {
+  describe('change organization owner / behavior ', () => {
+    let outwave
+    let keyPrice = web3.utils.toWei('0.01', 'ether')
+    let addr1
+    const baseTokenUri = 'https://uri.com/'
+
+    before(async () => {
+      let addresses = await require('../helpers/deploy').deployUnlock('10')
+      let outwaveFactory = await ethers.getContractFactory('OutwaveEvent')
+      let outwaveManager = await outwaveFactory.attach(addresses.outwaveAddress)
+      outwave = await ethers.getContractAt(
+        'IEventOrganizationManagerMixin',
+        addresses.outwaveAddress
+      )
+      await outwaveManager.setBaseTokenUri(baseTokenUri)
+      await ethers.getSigners()
+    })
+
+    it('should successfully change organization owner', async () => {
+      ;[, addr1] = await ethers.getSigners()
+      const tx = await outwave.connect(addr1).eventCreate(
+        web3.utils.padLeft(web3.utils.asciiToHex('1'), 64),
+        'name',
+        web3.utils.padLeft(0, 40), // address(0)
+        keyPrice,
+        100000,
+        100,
+        web3.utils.padLeft(web3.utils.asciiToHex('2'), 64)
+      )
+      let receipt = await tx.wait()
+
+      let eventCreate = receipt.events.find((v) => v.event === 'EventCreated')
+      assert.equal(
+        eventCreate.args.eventId,
+        web3.utils.padLeft(web3.utils.asciiToHex('1'), 64)
+      )
+      assert.equal(eventCreate.args.owner, addr1.address)
+
+      // change owner and performs checks
+      assert.isTrue(await outwave.organizationIsOwned(eventCreate.args.owner))
+
+      const newOwner = '0x2CAF3950d36D92165dc7b51DCeA3f1314cE20B84'
+      outwave.organizationChangeOwner(eventCreate.args.owner, newOwner)
+
+      assert.isTrue(await outwave.organizationIsOwned(newOwner))
+      assert.isFalse(await outwave.organizationIsOwned(eventCreate.args.owner))
+    })
+  })
+})
