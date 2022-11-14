ec8c0b7e5ca97b87029644c8d696f88717ad90a1
Giacomo Stelluti Scala <gsscoder@gmail.com>
2022-11-14 14:18:52 +0100

    Reorganize source files

--- contracts/utils/ERC721EnumerableUpgradeable.sol
+++ contracts/utils/ERC721EnumerableUpgradeable.sol
@@ -1,7 +1,7 @@
+// based on OpenZeppelin Contracts v4.4.1 (token/ERC721/extensions/ERC721Enumerable.sol)
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts v4.4.1 (token/ERC721/extensions/ERC721Enumerable.sol)
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/IERC721ReceiverUpgradeable.sol";
